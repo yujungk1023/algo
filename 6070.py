c44f0aafd27e9bbdd57c0998d25bf344fdc40b7e
DESKTOP-P4QC55J\곽유정 <yujungk1023@gmail.com>
2023-08-13 17:26:57 +0900

    add 6065~6070.py

---
+++ 6070.py
@@ -0,0 +1,33 @@
+# 본 문제는 python 의 빠른 기초 학습을 위해 설계된 문제로서 python 코드 제출을 기준으로 설명되어 있습니다. 
+# ------
+
+# 월이 입력될 때 계절 이름이 출력되도록 해보자.
+
+# 월 : 계절 이름
+# 12, 1, 2 : winter
+#   3, 4, 5 : spring
+#   6, 7, 8 : summer
+#   9, 10, 11 : fall
+
+# 예시
+# ...
+# if n//3==1 :
+#   print("spring")
+# ...
+
+# 참고
+# 때때로 수들의 특징을 관찰하고 이용하면 매우 간단히 해결할 수도 있다.
+
+x = input()
+x = int(x)
+if x//3 == 1 :
+    print("spring")
+else :
+    if x//3 == 2 :
+        print("summer")
+    else :
+        if x//3 == 3 :
+            print("fall")
+        else :
+            if (x//3 == 4) or (x//3 == 0) :
+                print("winter")
